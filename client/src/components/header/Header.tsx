4e0f2551b3efc1ad21f0a323c9402e84e9bf2bdb
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-02-12 20:11:52 +0100

    style: header discoverypage

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,15 +1,25 @@
+import style from "./header.module.css";
+
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <img src="/image/logo.webp" alt="logo Greenshare" />
-        <section>
-          <button type="button">se connecter</button>
-          <button type="button">S'inscrire</button>
+    <header className={style.header}>
+      <nav className={style.nav}>
+        <img
+          src="/image/logo.webp"
+          alt="logo Greenshare"
+          className={style.logo}
+        />
+        <section className={style.boutton}>
+          <button type="button" className={style.btn}>
+            se connecter
+          </button>
+          <button type="button" className={style.btn}>
+            S'inscrire
+          </button>
         </section>
       </nav>
       <section>
-        <h1>GREENSHARE</h1>
+        <h1 className={style.title}>GREENSHARE</h1>
       </section>
     </header>
   );
