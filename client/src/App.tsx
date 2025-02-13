1fa61eacbb413f2908e8d96296809d2be1c0a9ce
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-02-13 08:27:15 +0100

    feat: create header homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import HeaderHomePage from "./headerHomePage/HeaderHomePage";
 
 function App() {
   return (
     <>
+      <HeaderHomePage />
       <Outlet />
     </>
   );
