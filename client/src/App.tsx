4ca54ec8c267ff9526d1d4addc783350d599cbb6
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-02-13 21:38:35 +0100

    feat: create and style footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 import HeaderHomePage from "./components/headerHomePage/HeaderHomePage";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <HeaderHomePage />
       <Outlet />
+      <Footer />
     </>
   );
 }
--- client/src/pages/discoveryPage/DiscoveryPage.tsx
+++ client/src/pages/discoveryPage/DiscoveryPage.tsx
@@ -1,11 +1,12 @@
+import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Headerdiscovery";
 import TeaserMain from "../../components/teaser/TeaserMain";
-
 export default function DiscoveryPage() {
   return (
     <>
       <Header />
       <TeaserMain />
+      <Footer />
     </>
   );
 }
