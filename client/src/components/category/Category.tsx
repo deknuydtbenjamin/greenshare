fa07e0c3839e119a69c3aba35d452f81f71732c2
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-02-14 09:07:14 +0100

    feat:read category admin page

--- client/src/components/category/Category.tsx
+++ client/src/components/category/Category.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import type { SubmitHandler } from "react-hook-form";
 import { ToastContainer, toast } from "react-toastify";
@@ -24,33 +25,59 @@ export default function Category() {
     }
   };
 
+  const [category, setCategory] = useState<CategoryType[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/api/category`,
+          {},
+        );
+        setCategory(response.data);
+      } catch (error) {
+        toast.error("Impossible de charger les données.", {});
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <section className={style.category}>
-      <ToastContainer />
-      <form onSubmit={handleSubmit(formSubmit)} className={style.form}>
-        <label className={style.champ}>
-          Nom de la categorie
-          <input
-            className={style.bloc}
-            type="text"
-            {...register("label", {
-              required: "champ obligatoire",
-              minLength: {
-                value: 2,
-                message: "le champ doit contenir au minimum 3 caractères",
-              },
-              maxLength: {
-                value: 30,
-                message: "le champ doit contenir au maximun 30 caractères",
-              },
-            })}
-          />
-          {errors.label && <p>{errors.label.message}</p>}
-        </label>
-        <button type="submit" className={style.btn}>
-          Ajouter
-        </button>
-      </form>
+    <section>
+      <section className={style.category}>
+        <ToastContainer />
+        <form onSubmit={handleSubmit(formSubmit)} className={style.form}>
+          <label className={style.champ}>
+            Nom de la categorie
+            <input
+              className={style.bloc}
+              type="text"
+              {...register("label", {
+                required: "champ obligatoire",
+                minLength: {
+                  value: 2,
+                  message: "le champ doit contenir au minimum 3 caractères",
+                },
+                maxLength: {
+                  value: 30,
+                  message: "le champ doit contenir au maximun 30 caractères",
+                },
+              })}
+            />
+            {errors.label && <p>{errors.label.message}</p>}
+          </label>
+          <button type="submit" className={style.btn}>
+            Ajouter
+          </button>
+        </form>
+      </section>
+      <section className={style.card}>
+        {category.map((c) => (
+          <div key={c.id} className={style.cartrole}>
+            <p>{c.label}</p>
+          </div>
+        ))}
+      </section>
     </section>
   );
 }
