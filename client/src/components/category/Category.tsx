5d7f6828b07513e4d372579f03d9d6969c200222
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-02-14 09:44:06 +0100

    fix: category

--- client/src/components/category/Category.tsx
+++ client/src/components/category/Category.tsx
@@ -12,7 +12,6 @@ export default function Category() {
     handleSubmit,
     formState: { errors },
   } = useForm<CategoryType>();
-  // const [categories, setCategories] = useState<CategoryType[]>([]);
 
   const formSubmit: SubmitHandler<CategoryType> = async (data) => {
     try {
@@ -55,6 +54,16 @@ export default function Category() {
       toast.error("Erreur lors de la suppression.");
     }
   };
+  const editSubmit: SubmitHandler<CategoryType> = async (categoryId) => {
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_API_URL}/api/category/${categoryId}`,
+      );
+      toast.success(response.data.message, {});
+    } catch (err) {
+      toast.error("Erreur lors de la mise à jour de la catégorie", {});
+    }
+  };
 
   return (
     <section>
@@ -95,6 +104,13 @@ export default function Category() {
               onClick={() => handleDelete(c.id)}
             >
               Supprimer le role
+            </button>
+            <button
+              type="button"
+              className={style.btnedit}
+              onClick={() => editSubmit(c)}
+            >
+              Modifier
             </button>
           </div>
         ))}
